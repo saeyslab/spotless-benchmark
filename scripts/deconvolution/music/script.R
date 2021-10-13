46a905541f4ea74a753afe8ad53c5afcec537f82
csangara <sai.chananchida@gmail.com>
2021-10-13 11:10:02 +0200

    viash files for music :notes:

---
+++ scripts/deconvolution/music/script.R
@@ -0,0 +1,58 @@
+library(Seurat)
+library(MuSiC)
+library(xbioc, quietly=TRUE)
+
+## VIASH START
+par <- list(
+  sc_input = "/mnt/d/spade-benchmark/unit-test/test_sc_data.rds",
+  sp_input = "/mnt/d/spade-benchmark/unit-test/test_sp_data.rds",
+  annot = "subclass",
+  output = "proportions.tsv",
+  sampleID = "none"
+)
+## VIASH END
+
+# Read and preprocess single-cell RNA data
+cat("Reading input scRNA-seq reference...")
+seurat_obj_scRNA <- readRDS(par$sc_input)
+ncelltypes <- length(unique(seurat_obj_scRNA[[par$annot, drop=TRUE]]))
+cat("Found ", ncelltypes, "cell types in the reference.\n")
+
+# Check if sample annotation is given and if it exists in the metadata
+if (par$sampleID == "none" || ! par$sampleID %in% colnames(seurat_obj_scRNA@meta.data)){
+  sampleIDs <- colnames(seurat_obj_scRNA)
+} else {
+  sampleIDs <- seurat_obj_scRNA[[par$sampleID, drop=TRUE]]
+}
+
+# Convert to ExpressionSet object
+cat("Converting to ExpressionSet object...\n")
+sc.pdata <- new("AnnotatedDataFrame",
+                data = data.frame(
+                celltype = seurat_obj_scRNA[[par$annot, drop=TRUE]],
+                samples = sampleIDs))
+sc.data <- as.matrix(Seurat::GetAssayData(seurat_obj_scRNA))
+eset_obj_scRNA <- Biobase::ExpressionSet(assayData=sc.data, phenoData=sc.pdata)
+
+# Run MuSiC
+cat("Reading input spatial data...\n")
+synthetic_visium_data <- readRDS(par$sp_input)
+
+cat("Converting spatial data to ExpressionSet object...\n")
+eset_obj_visium <- Biobase::ExpressionSet(assayData=as.matrix(synthetic_visium_data$counts))
+
+cat("Running deconvolution tool...\n")
+start_time <- Sys.time()
+music_deconv = MuSiC::music_prop(bulk.eset = eset_obj_visium, sc.eset = eset_obj_scRNA,
+                                 clusters = 'celltype', samples = 'samples')
+end_time <- Sys.time()
+cat("Runtime: ", round((end_time-start_time)[[1]], 2), "s\n", sep="")
+
+cat("Printing results...\n")
+deconv_matrix <- music_deconv$Est.prop.weighted[,1:ncelltypes]
+
+# Remove all spaces and dots from cell names, sort them
+colnames(deconv_matrix) <- stringr::str_replace_all(colnames(deconv_matrix), "[/ .]", "")
+deconv_matrix <- deconv_matrix[,sort(colnames(deconv_matrix), method="shell")]
+
+write.table(deconv_matrix, file=par$output, sep="\t", quote=FALSE, row.names=FALSE)
