886aa3e9f559bbae1b242c5c094c1a173c1eff60
csangara <sai.chananchida@gmail.com>
2021-11-24 16:01:58 +0100

    separated different sections to different folders

--- scripts/data_generation/preprocess_seqFISH.R
+++ scripts/data_generation/preprocess_seqFISH.R
@@ -37,6 +37,7 @@ get_coarse_annot <- function(celltype){
 path <- "D:/spade-benchmark/data/seqFISH_eng2019/"
 dataset_source <- "Eng2019"
 dataset <- "ob" # cortex_svz or ob
+standard_no <- ifelse(dataset == "cortex_svz", 1, 2)
 fov_no <- 2 # 0-6
 
 #### READ IN FILE ####
@@ -68,7 +69,7 @@ if (dataset == "ob"){
 library(Seurat)
 seurat_obj <- CreateSeuratObject(counts=t(counts[metadata$celltype != "Unannotated",]),
                                  meta.data=metadata[metadata$celltype != "Unannotated",])
-saveRDS(seurat_obj, paste0("D:/spade-benchmark/data/gold_standard/reference/reference_", dataset, ".rds"))
+saveRDS(seurat_obj, paste0("D:/spade-benchmark/data/gold_standard_", standard_no, "/reference/reference_", dataset, ".rds"))
 # seurat_obj <- seurat_obj %>% ScaleData() %>% FindVariableFeatures() %>%
 #   RunPCA() %>%  RunUMAP(dims = 1:30)
 # DimPlot(seurat_obj, reduction = "umap", group.by="celltype_coarse", label=TRUE)
@@ -168,7 +169,7 @@ for (fov_no in 0:6){
   
   # Save rds
   filename <- paste0(dataset_source, "_", dataset, "_fov", fov_no)
-  saveRDS(full_data, paste0("D:/spade-benchmark/data/gold_standard/", filename, ".rds"))
+  saveRDS(full_data, paste0("D:/spade-benchmark/data/gold_standard_", standard_no, "/", filename, ".rds"))
   
   # Save plot and add additional information on cells, celltypes and counts
   plot_table <- cells_in_spots %>% group_by(spot_no) %>%
@@ -176,7 +177,7 @@ for (fov_no in 0:6){
     add_column(total_counts=rowSums(spot_counts)) %>% column_to_rownames("spot_no")
   p_final <- grid.arrange(p + coord_fixed() + ggtitle(filename) + scale_y_reverse(),
                           tableGrob(plot_table), widths=c(2,1))
-  ggsave(paste0("D:/spade-benchmark/data/gold_standard/", filename, ".jpeg"),
+  ggsave(paste0("D:/spade-benchmark/data/gold_standard_", standard_no, "/", filename, ".jpeg"),
          p_final, width = 3000, height = 1600, units="px")
 }
 
