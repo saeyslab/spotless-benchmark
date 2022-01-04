05a208fa18782e9cffb8b726704bce81c061cac4
csangara <sai.chananchida@gmail.com>
2022-01-04 17:56:01 +0100

    Update seqFISH_dataset_properties.R

--- scripts/seqFISH_dataset_properties.R
+++ scripts/seqFISH_dataset_properties.R
@@ -3,6 +3,10 @@ library(Seurat)
 library(ggplot2)
 library(dplyr)
 scRNA_obj <- readRDS("D:/spade-benchmark/standards/reference/gold_standard_1.rds")
+scRNA_obj <- scRNA_obj %>% ScaleData() %>% FindVariableFeatures() %>%
+  RunPCA() %>%  RunUMAP(dims = 1:30)
+DimPlot(scRNA_obj, reduction = "umap", group.by="celltype_coarse", label=TRUE)
+
 VlnPlot(scRNA_obj, features="nCount_RNA", group.by = "celltype_coarse")
 df <- data.frame(scRNA_obj@meta.data)
 df <- df %>% select(c("nCount_RNA", "nFeature_RNA")) %>% reshape2::melt(id.var=NULL)
@@ -10,6 +14,10 @@ ggplot(data=df, aes(x=value, color=variable)) + geom_density()
 
 
 scRNA_obj <- readRDS("D:/spade-benchmark/standards/reference/gold_standard_2.rds")
+scRNA_obj <- scRNA_obj %>% ScaleData() %>% FindVariableFeatures() %>%
+  RunPCA() %>%  RunUMAP(dims = 1:30)
+DimPlot(scRNA_obj, reduction = "umap", group.by="celltype_coarse", label=TRUE)
+
 VlnPlot(scRNA_obj, features="nCount_RNA", group.by = "celltype_coarse")
 df <- data.frame(scRNA_obj@meta.data)
 df <- df %>% select(c("nCount_RNA", "nFeature_RNA")) %>% reshape2::melt(id.var=NULL)
