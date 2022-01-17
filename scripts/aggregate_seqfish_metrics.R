6300b2073bcd867c7ce63ac03bc322af84a81f8a
csangara <sai.chananchida@gmail.com>
2022-01-17 17:04:53 +0100

    Update aggregate_seqfish_metrics.R
    more plots for gold standard results

--- scripts/aggregate_seqfish_metrics.R
+++ scripts/aggregate_seqfish_metrics.R
@@ -133,7 +133,7 @@ ggplot(df, aes(y=value, x=method, colour=method)) +
   facet_grid(metric ~ dataset, scales = "free_y", switch="y",
              labeller=labeller(dataset=proper_dataset_names, metric=proper_metric_names))
 
-ggsave("D:/PhD/figs/benchmark_paper/gold_standard.png", units="px", width=1600, height=1000)
+ggsave("D:/PhD/figs/benchmark_paper/gold_standard_a.png", units="px", width=1600, height=1000)
 
 ### BEST PERFORMERS ###
 df %>% filter(metric == "RMSE") %>% group_by(method, dataset) %>%
@@ -165,6 +165,90 @@ ps[[1]] + ps[[2]] + plot_layout(guides='collect') & theme(legend.position="botto
 
 ggsave("D:/PhD/figs/benchmark_paper/gold_standard_group.png", units="px", width=1800, height=900)
 
+### BARPLOT ###
+results <- lapply(c("cortex_svz", "ob"), function (dataset) {
+  lapply(methods, function (method) {
+    lapply(fovs, function(fov){
+      read.table(paste0("D:/spade-benchmark/deconv_proportions/Eng2019_", dataset, "/proportions_", method,
+                        "_Eng2019_", dataset, "_fov", fov),
+                 header = TRUE, sep= "\t")
+      }) %>%
+      setNames(fovs) %>% melt(id.vars=NULL) %>%
+      `colnames<-`(c("celltype", "proportion", "fov")) %>%
+      mutate(method = method)}) %>%
+    do.call(rbind, .) %>% mutate("dataset" = dataset)
+}) %>% do.call(rbind, .)
 
 
+ground_truth <- lapply(1:2, function (i) {
+  reference <- readRDS(paste0("D:/spade-benchmark/standards/reference/gold_standard_", i, ".rds"))
+  celltypes <- stringr::str_replace_all(unique(reference$celltype), "[/ .]", "")
+    lapply(fovs, function(fov){
+      known_props <- readRDS(paste0("D:/spade-benchmark/standards/gold_standard_",
+                                    i, "/Eng2019_", datasets[i],
+                                    "_fov", fov, ".rds"))$relative_spot_composition
+      colnames(known_props) <- stringr::str_replace_all(colnames(known_props), "[/ .]", "")
+      columns_to_add <- celltypes[!celltypes %in% colnames(known_props)]
+      known_props <- cbind(known_props,
+                           matrix(0, nrow=nrow(known_props), ncol=length(columns_to_add),
+                                  dimnames = list(rownames(known_props), columns_to_add)))
+      known_props <- known_props[,sort(colnames(known_props), method="shell")] 
+      known_props <- known_props[,-(length(celltypes)+1)]
+      }) %>%
+      setNames(fovs) %>%
+       melt(id.vars=NULL) %>% mutate(method = "known", dataset = datasets[i]) %>%
+      `colnames<-`(c("celltype", "proportion", "fov", "method", "dataset"))}) %>%
+     do.call(rbind, .)
 
+combined <- rbind(results, ground_truth)
+combined_summ <- combined %>% group_by(dataset, fov, method, celltype) %>%
+  summarise(mean_props = sum(as.numeric(proportion))) %>% ungroup %>%
+  mutate(method = factor(method, levels=c("known", methods)))
+
+# For coarse dataset
+combined_summ_coarse <- combined_summ %>% ungroup() %>%
+  # Group excitatory neurons together, and interneurons together
+  mutate(celltype = sapply(as.character(celltype), get_coarse_annot)) %>%
+  group_by(dataset, fov, method, celltype) %>% summarise(mean_props = sum(mean_props))
+
+proper_dataset_names <- c("Cortex", "Olfactory Bulb") %>% setNames(c("cortex_svz", "ob"))
+proper_method_names <- c("Known", "Cell2location", "MuSiC", "RCTD", "SPOTlight", "stereoscope")
+
+library(RColorBrewer)
+plots <- list()
+for (ds in datasets) {
+  
+  n <- combined_summ_coarse %>% filter(dataset==ds) %>% ungroup() %>% select(celltype) %>% unique() %>% nrow()
+  qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+  col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+  
+  p <- ggplot(subset(combined_summ_coarse, dataset==ds),
+         aes(x=method, y=mean_props, fill=celltype)) +
+      geom_bar(width=0.4, stat="identity", position=position_stack(reverse=TRUE)) +
+      scale_x_discrete(limits = rev(levels(combined_summ$method)),
+                       labels = rev(proper_method_names)) +
+      scale_fill_manual(values=col_vector) +
+      facet_wrap(~fov, nrow=1) +
+      coord_flip() + 
+      ylab("Sum of proportions across all spots in a FOV") +
+      labs(fill="Cell type") + theme_bw() +
+      theme(axis.ticks.x = element_blank(), axis.text.x = element_blank(),
+            axis.title = element_blank(), panel.grid = element_blank(),
+            strip.background = element_rect(fill = "white"))
+  if (ds == "cortex_svz") { p <- p + guides(fill=guide_legend(ncol=2))}
+  plots[[ds]] <- p
+  
+}
+
+p_all <- patchwork::wrap_plots(plots, nrow = 2)
+p_all
+ggsave("D:/spade-benchmark/plots/seqFISH_abundance_barplot_a.png",
+       p_all, width=297, height=120, units="mm", dpi=200)
+
+#### Coarse bar plot
+get_coarse_annot <- function(celltype){
+  conditions <- c(grepl("Excitatorylayer", celltype), grepl("Interneuron", celltype))
+  replacements <- c('Excitatoryneurons', 'Interneurons')
+  if (all(!conditions)) { return (celltype) }
+  else { return (replacements[which(conditions)] )}
+}
